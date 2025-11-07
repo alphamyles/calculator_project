6a28336e56a3b61dab6b9b6400cdffe057596a5b
alfredlamptey <alphamyles97@gmail.com>
2025-11-07 14:50:24 +0100

    Adding all operations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -7,13 +12,9 @@ public class Main {
         System.out.print("Enter a number:");
         int firstNumber = scanner.nextInt();
         System.out.print("Operation: ");
-        String addition = "+";
-        String subtraction = "-";
-        String multiplication = "*";
-        String division = "/";
         String userInput = scanner.next();
-
-        while (!Objects.equals(userInput, addition)&&!Objects.equals(userInput,subtraction)){
+        List<String> options = Arrays.asList("+","-","*","/");
+        while (!options.contains(userInput)){
            System.out.println("invalid operation");
            System.out.print("Operation: ");
             userInput = scanner.next();
@@ -27,6 +28,17 @@ public class Main {
            System.out.print("Enter another number:");
            secondNumber = scanner.nextInt();
            int finalResult = firstNumber - secondNumber;
+           System.out.println(finalResult);
+       }else if (Objects.equals(userInput, "*")){
+           System.out.print("Enter another number:");
+           secondNumber = scanner.nextInt();
+           int finalResult = firstNumber * secondNumber;
+           System.out.println(finalResult);
+
+       }else if (Objects.equals(userInput, "/")){
+           System.out.print("Enter another number:");
+           secondNumber = scanner.nextInt();
+           int finalResult = firstNumber / secondNumber;
            System.out.println(finalResult);
 
        }
