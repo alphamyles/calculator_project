49887a9c0de3f2c2fb47f3518fc25e67df7ff706
alfred.lamptey <alphamyles97@gmail.com>
2025-11-13 21:07:26 +0100

    Changing logic by using methods

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int secondNumber ;
+       Scanner scanner = new Scanner(System.in);
+      userInputNumber(scanner);
+      operation(scanner);
+      userInputSecondNumber(scanner);
+    }
+    
+    public static void userInputNumber(Scanner scanner) {
         System.out.print("Enter a number:");
+        scanner.nextInt();
+
+    }
+    public static void operation(Scanner scanner) {
+        System.out.print("Select Operation:");
+        scanner.nextInt();
+
+    }
+    public static void userInputSecondNumber(Scanner scanner) {
+        System.out.print("Enter another number:");
+        scanner.nextInt();
+
+    }
+
+}
+       /* System.out.print("Enter a number:");
         int firstNumber = scanner.nextInt();
         System.out.print("Operation: ");
         String userInput = scanner.next();
@@ -19,34 +38,42 @@ public class Main {
            System.out.print("Operation: ");
             userInput = scanner.next();
        }
-       if (Objects.equals(userInput, "+")){
-           System.out.print("Enter another number:");
-           secondNumber = scanner.nextInt();
-           int finalResult = firstNumber + secondNumber;
-           System.out.println(finalResult);
-       }else if (Objects.equals(userInput, "-")){
-           System.out.print("Enter another number:");
-           secondNumber = scanner.nextInt();
-           int finalResult = firstNumber - secondNumber;
-           System.out.println(finalResult);
-       }else if (Objects.equals(userInput, "*")){
-           System.out.print("Enter another number:");
-           secondNumber = scanner.nextInt();
-           int finalResult = firstNumber * secondNumber;
-           System.out.println(finalResult);
+        switch (userInput) {
+            case "+" -> {
+                System.out.print("Enter another number:");
+                secondNumber = scanner.nextInt();
+                int finalResult = firstNumber + secondNumber;
+                System.out.println(finalResult);
+            }
+            case "-" -> {
+                System.out.print("Enter another number:");
+                secondNumber = scanner.nextInt();
+                int finalResult = firstNumber - secondNumber;
+                System.out.println(finalResult);
+            }
+            case "*" -> {
+                System.out.print("Enter another number:");
+                secondNumber = scanner.nextInt();
+                int finalResult = firstNumber * secondNumber;
+                System.out.println(finalResult);
 
-       }else if (Objects.equals(userInput, "/")){
-           System.out.print("Enter another number:");
-           secondNumber = scanner.nextInt();
-           int finalResult = firstNumber / secondNumber;
-           System.out.println(finalResult);
+            }
+            case "/" -> {
+                System.out.print("Enter another number:");
+                secondNumber = scanner.nextInt();
+                int finalResult = firstNumber / secondNumber;
+                System.out.println(finalResult);
 
-       }
+            }
+            case null, default -> {
+            }
+        }
 
 
         scanner.close();
         }
+        }
+*/
 
 
 
-    }
