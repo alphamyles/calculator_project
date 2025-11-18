513644d6e244179773a7d0b387e4423d43dfd485
alfredlamptey <alphamyles97@gmail.com>
2025-11-18 20:51:54 +0100

    creating the addition function

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,48 @@ import java.util.Scanner;
 
 
 public class Main {
-
+    //introducing class level variables to efficiently use methods
+     static String sign;
+     static int firstNumber;
+     static int secondNumber;
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-     int firstNumber = userInputNumber(scanner);
-     String sign = operation(scanner);
-
-    while(!Objects.equals(sign, "+")) {
-        System.out.println("invalid operation");
+        firstNumber = userInputNumber(scanner);
         sign = operation(scanner);
+        //putting the method in a variable to be able to use it
+        int a = addition();
+        System.out.println(a);
+        }
+        public static int addition(){
+            while (!Objects.equals(sign, "+")) {
+                System.out.println("invalid operation");
+                //not putting the operation in a variable causes problems
+                sign = operation(scanner);
+            }
+            if (sign.equals("+"))
+                secondNumber = userInputSecondNumber(scanner);
+            return firstNumber + secondNumber;
+        }
+        public static int userInputNumber (Scanner scanner){
+            System.out.print("Enter a number:");
+            return scanner.nextInt();
+
+        }
+        public static String operation (Scanner scanner){
+            System.out.print("Select Operation:");
+
+            return scanner.next();
+
+        }
+        public static int userInputSecondNumber (Scanner scanner){
+            System.out.print("Enter another number:");
+            return scanner.nextInt();
+
+        }
+
     }
-     if (Objects.equals(sign, "+")){
-            int secondNumber = userInputSecondNumber(scanner);
-            int result = firstNumber + secondNumber;
-            System.out.println(result);
-         }
-     }
-
-    public static int userInputNumber(Scanner scanner) {
-        System.out.print("Enter a number:");
-        return scanner.nextInt();
-        
-    }
-    public static String operation(Scanner scanner) {
-        System.out.print("Select Operation:");
-
-      return scanner.next();
-
-    }
-    public static int userInputSecondNumber(Scanner scanner) {
-        System.out.print("Enter another number:");
-       return scanner.nextInt();
-
-    }
 
 
-}
 
        /* System.out.print("Enter a number:");
         int firstNumber = scanner.nextInt();
